9a2ee7ef279e765b80a1bba5fe83c717af300960
noah-hein <noahhein123@gmail.com>
2023-08-14 22:55:33 -0400

    Added okhttp library

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     implementation("org.projectlombok:lombok:1.18.26")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
