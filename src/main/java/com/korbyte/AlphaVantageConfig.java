d0e309daa12ef8f74f234b73be40a742217d4c82
noah-hein <noahhein123@gmail.com>
2023-08-15 00:47:03 -0400

    Removed whitespace in config

--- src/main/java/com/korbyte/AlphaVantageConfig.java
+++ src/main/java/com/korbyte/AlphaVantageConfig.java
@@ -4,13 +4,8 @@ import lombok.Data;
 
 @Data
 public class AlphaVantageConfig {
-
   private String apiKey;
-
   private String protocol = "https";
-
   private String host = "www.alphavantage.co";
-
   private String path = "/query";
-
 }
