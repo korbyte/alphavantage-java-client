ed76958f051d9bc96917a1da565ec8a7fc9106e5
Gabe Nelson <unofficialwood@gmail.com>
2023-08-15 22:45:59 -0400

    added Daily and DailyAdjustedApi

---
+++ src/main/java/com/korbyte/alphavantage/core/daily/DailyAdjustedApi.java
@@ -0,0 +1,31 @@
+package com.korbyte.alphavantage.core.daily;
+
+import com.korbyte.alphavantage.AlphaVantageClient;
+import com.korbyte.alphavantage.Configuration;
+import com.korbyte.alphavantage.base.ApiFunction;
+import com.korbyte.alphavantage.base.ApiInterface;
+import com.korbyte.alphavantage.models.QueryParams;
+import com.korbyte.alphavantage.models.core.daily.DailyAdjustedResponse;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class DailyAdjustedApi extends AlphaVantageClient implements ApiInterface<String, DailyAdjustedResponse> {
+
+  private static final ApiFunction FUNCTION = ApiFunction.DAILY;
+
+  private final Class<DailyAdjustedResponse> responseClass = DailyAdjustedResponse.class;
+
+  public DailyAdjustedApi(Configuration config) {
+    super(config);
+  }
+
+  public DailyAdjustedResponse get(String tickerSymbol) throws URISyntaxException, IOException {
+    //todo: figure out rest of the params
+    QueryParams req = new QueryParams();
+    req.setApiFunction(FUNCTION);
+    req.setSymbol(this.formatTickerSymbol(tickerSymbol));
+
+    return this.query(responseClass, req);
+  }
+}
---
+++ src/main/java/com/korbyte/alphavantage/core/daily/DailyApi.java
@@ -0,0 +1,31 @@
+package com.korbyte.alphavantage.core.daily;
+
+import com.korbyte.alphavantage.AlphaVantageClient;
+import com.korbyte.alphavantage.Configuration;
+import com.korbyte.alphavantage.base.ApiFunction;
+import com.korbyte.alphavantage.base.ApiInterface;
+import com.korbyte.alphavantage.models.QueryParams;
+import com.korbyte.alphavantage.models.core.daily.DailyResponse;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class DailyApi extends AlphaVantageClient implements ApiInterface<String, DailyResponse> {
+
+  private static final ApiFunction FUNCTION = ApiFunction.DAILY;
+
+  private final Class<DailyResponse> responseClass = DailyResponse.class;
+
+  public DailyApi(Configuration config) {
+    super(config);
+  }
+
+  public DailyResponse get(String tickerSymbol) throws URISyntaxException, IOException {
+    //todo: figure out rest of the params
+    QueryParams req = new QueryParams();
+    req.setApiFunction(FUNCTION);
+    req.setSymbol(this.formatTickerSymbol(tickerSymbol));
+
+    return this.query(responseClass, req);
+  }
+}
