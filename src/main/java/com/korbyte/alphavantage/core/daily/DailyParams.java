39624c393eb3f2b312a438774b37ad44716cd2a4
hein.66 <hein.66@osu.edu>
2023-08-16 15:07:03 -0400

    Fixed params for daily service

--- src/main/java/com/korbyte/alphavantage/core/daily/DailyParams.java
+++ src/main/java/com/korbyte/alphavantage/core/daily/DailyParams.java
@@ -3,16 +3,11 @@ package com.korbyte.alphavantage.core.daily;
 import com.korbyte.alphavantage.ApiFunction;
 import lombok.Data;
 
-@Data()
+@Data
 public class DailyParams {
-
   private ApiFunction function;
-
   private String apiKey;
-
   private String symbol;
-
   private String outputSize;
-
   private String datatype;
 }
