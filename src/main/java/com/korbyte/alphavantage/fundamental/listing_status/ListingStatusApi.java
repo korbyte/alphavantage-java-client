7698826275eda08edf06030828d2d4294040baac
Noah Hein <noahhein123@gmail.com>
2023-08-16 21:04:57 -0400

    Fixed the listing status api

--- src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusApi.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusApi.java
@@ -2,21 +2,72 @@ package com.korbyte.alphavantage.fundamental.listing_status;
 
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
-import com.opencsv.CSVParser;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ListingStatusApi extends AlphaVantageApi {
   public ListingStatusApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
   }
 
-  public void get(ListingStatusParams params) throws URISyntaxException, IOException {
+  public List<ListingStatusResponse> get(ListingStatusParams params)
+    throws URISyntaxException, IOException, CsvValidationException, ParseException
+  {
     String data = this.query(params);
-    CSVParser parser = new CSVParser();
+    return parseCSV(data);
+  }
 
-    System.out.println(data);
+  private List<ListingStatusResponse> parseCSV(String data) throws ParseException, CsvValidationException, IOException {
+    CSVReader reader = new CSVReader(new StringReader(data));
+    List<ListingStatusResponse> listingStatusResponses = new ArrayList<>();
+
+    String[] line;
+    reader.skip(1);
+    while ((line = reader.readNext()) != null) {
+      ListingStatusResponse listingStatusResponse = new ListingStatusResponse();
+
+      String symbol = line[0];
+      String name = line[1];
+      String exchange = line[2];
+      String assetType = line[3];
+      Date ipoDate = parseDateString(line[4]);
+      Date delistDate = parseDateString(line[5]);
+      Boolean status = line[6].equals("true");
+
+      listingStatusResponse
+        .setSymbol(symbol)
+        .setName(name)
+        .setExchange(exchange)
+        .setAssetType(assetType)
+        .setIopDate(ipoDate)
+        .setDelistingDate(delistDate)
+        .setStatus(status);
+
+      listingStatusResponses.add(listingStatusResponse);
+    }
+    return listingStatusResponses;
+  }
+
+  private void test() {
+
+  }
+
+  private Date parseDateString(String dateString) {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Date date = null;
+    try {
+      date = sdf.parse(dateString);
+    } catch (ParseException ignored) {}
+    return date;
   }
 }
--- src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusResponse.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusResponse.java
@@ -1,6 +1,18 @@
 package com.korbyte.alphavantage.fundamental.listing_status;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Data
+@Accessors(chain = true)
 public class ListingStatusResponse {
-
-
+  private String symbol;
+  private String name;
+  private String exchange;
+  private String assetType;
+  private Date iopDate;
+  private Date delistingDate;
+  private Boolean status;
 }
