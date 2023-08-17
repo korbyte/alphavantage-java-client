b52fee2b3485f7ae529059cc18e77ef7e306a9f0
Gabe Nelson <unofficialwood@gmail.com>
2023-08-17 19:16:07 -0400

    added weekly to core api

--- src/main/java/com/korbyte/alphavantage/core/CoreApi.java
+++ src/main/java/com/korbyte/alphavantage/core/CoreApi.java
@@ -3,6 +3,7 @@ package com.korbyte.alphavantage.core;
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
 import com.korbyte.alphavantage.core.daily.DailyApi;
+import com.korbyte.alphavantage.core.weekly.WeeklyApi;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import okhttp3.OkHttpClient;
@@ -12,9 +13,11 @@ import okhttp3.OkHttpClient;
 public class CoreApi extends AlphaVantageApi {
 
   private final DailyApi daily;
+  private final WeeklyApi weekly;
 
   public CoreApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
     this.daily = new DailyApi(config, client);
+    this.weekly = new WeeklyApi(config, client);
   }
 }
