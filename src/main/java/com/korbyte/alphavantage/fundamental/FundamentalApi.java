1d57266bd70c9be8badea0bf5c62f2400980f5ba
Gabe Nelson <nelson.1616@osu.edu>
2023-08-18 14:25:38 -0400

    added in CompanyOverviewApi to FundamentalApi

--- src/main/java/com/korbyte/alphavantage/fundamental/FundamentalApi.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/FundamentalApi.java
@@ -2,6 +2,7 @@ package com.korbyte.alphavantage.fundamental;
 
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
+import com.korbyte.alphavantage.fundamental.company_overview.CompanyOverviewApi;
 import com.korbyte.alphavantage.fundamental.listing_status.ListingStatusApi;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -12,9 +13,11 @@ import okhttp3.OkHttpClient;
 public class FundamentalApi extends AlphaVantageApi {
 
   private final ListingStatusApi listingStatus;
+  private final CompanyOverviewApi companyOverview;
 
   public FundamentalApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
     this.listingStatus = new ListingStatusApi(config, client);
+    this.companyOverview = new CompanyOverviewApi(config, client);
   }
 }
