beee09d65daa9a79449d599a691283bb0adc5bfa
Gabe Nelson <nelson.1616@osu.edu>
2023-08-18 13:23:02 -0400

    removed setters from function and added comments

--- src/main/java/com/korbyte/alphavantage/core/daily/models/adjusted/DailyAdjustedParams.java
+++ src/main/java/com/korbyte/alphavantage/core/daily/models/adjusted/DailyAdjustedParams.java
@@ -3,12 +3,36 @@ package com.korbyte.alphavantage.core.daily.models.adjusted;
 import com.korbyte.alphavantage.params.ApiFunction;
 import com.korbyte.alphavantage.params.DataType;
 import com.korbyte.alphavantage.params.OutputSize;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
+/**
+ * AlphaVantage Daily Adjusted request params model
+ */
 @Data
 public class DailyAdjustedParams {
+  /**
+   * By default, function=TIME_SERIES_DAILY_ADJUSTED.
+   * Does not change
+   */
+  @Setter(AccessLevel.NONE)
   private ApiFunction function = ApiFunction.TIME_SERIES_DAILY_ADJUSTED;
+
+  /**
+   * The name of the equity of your choice.
+   * For example, symbol=MSFT
+   */
   private String symbol;
+
+  /**
+   * By default, outputsize=compact.
+   */
   private OutputSize outputSize;
+
+  /**
+   * JSON or CSV.
+   * By default, datatype=json.
+   */
   private DataType datatype;
 }
--- src/main/java/com/korbyte/alphavantage/core/weekly/models/WeeklyParams.java
+++ src/main/java/com/korbyte/alphavantage/core/weekly/models/WeeklyParams.java
@@ -3,14 +3,23 @@ package com.korbyte.alphavantage.core.weekly.models;
 import com.korbyte.alphavantage.params.ApiFunction;
 import com.korbyte.alphavantage.params.DataType;
 import com.korbyte.alphavantage.params.OutputSize;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 /**
  * AlphaVantage Weekly request params model
  */
 @Data
 public class WeeklyParams {
+
+  /**
+   * By default, function=TIME_SERIES_WEEKLY.
+   * Does not change
+   */
+  @Setter(AccessLevel.NONE)
   private ApiFunction function = ApiFunction.TIME_SERIES_WEEKLY;
+
   /**
    * The name of the equity of your choice.
    * For example, symbol=MSFT
