9b01a1866a75e78cdda7801ac25096902ee11788
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 00:26:55 -0400

    added intraday api call

--- src/main/java/com/korbyte/alphavantage/core/intraday/IntradayApi.java
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayApi.java
@@ -1,11 +1,20 @@
 package com.korbyte.alphavantage.core.intraday;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
 import okhttp3.OkHttpClient;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class IntradayApi extends AlphaVantageApi {
   public IntradayApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
+  }
+
+  public IntradayResponse get(IntradayParams params) throws URISyntaxException, IOException {
+    String data = this.query(params);
+    return new ObjectMapper().readValue(data, IntradayResponse.class);
   }
 }
