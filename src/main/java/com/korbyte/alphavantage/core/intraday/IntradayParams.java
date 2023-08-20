a0540a4afd9bca040542ce48a8b911c9e31ca6e4
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 00:47:06 -0400

    comments and switch to enum for interval type

--- src/main/java/com/korbyte/alphavantage/core/intraday/IntradayParams.java
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayParams.java
@@ -3,18 +3,26 @@ package com.korbyte.alphavantage.core.intraday;
 import com.korbyte.alphavantage.params.ApiFunction;
 import com.korbyte.alphavantage.params.DataType;
 import com.korbyte.alphavantage.params.OutputSize;
+import com.korbyte.alphavantage.params.IntervalType;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
+/**
+ * Representation of the parameters for the intraday data API from alphavantage.co
+ * @see <a href="https://www.alphavantage.co/documentation/#intraday">https://www.alphavantage.co/documentation/#intraday</a>
+ */
 @Data
 public class IntradayParams {
 
+  @Setter(AccessLevel.NONE)
   private ApiFunction function = ApiFunction.TIME_SERIES_INTRADAY;
 
   private String apikey;
 
   private String symbol;
 
-  private String interval;
+  private IntervalType interval;
 
   private boolean adjusted;
 
