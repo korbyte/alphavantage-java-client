c293b13b1ea7883084d1ca592cd58fa383fac042
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 00:26:42 -0400

    added intraday response data

--- src/main/java/com/korbyte/alphavantage/core/intraday/IntradayResponse.java
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayResponse.java
@@ -1,4 +1,21 @@
 package com.korbyte.alphavantage.core.intraday;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
 public class IntradayResponse {
+
+  @JsonProperty("Meta Data")
+  IntradayMetadata metadata;
+
+  Map<String, IntradayPositionData> timeSeries;
+
+  @JsonAnySetter
+  public void setDynamicField(String key, Map<String, IntradayPositionData> value) {
+    this.timeSeries = value;
+  }
 }
