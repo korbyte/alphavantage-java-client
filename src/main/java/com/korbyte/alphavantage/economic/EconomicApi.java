a9e8f7527266f6cee548af09fc7e50e43ec0a4f4
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 18:26:11 -0400

    added gdp per captia to economic api

--- src/main/java/com/korbyte/alphavantage/economic/EconomicApi.java
+++ src/main/java/com/korbyte/alphavantage/economic/EconomicApi.java
@@ -3,6 +3,7 @@ package com.korbyte.alphavantage.economic;
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
 import com.korbyte.alphavantage.economic.gdp.GdpApi;
+import com.korbyte.alphavantage.economic.gdp_per_capita.GdpPerCapitaApi;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import okhttp3.OkHttpClient;
@@ -12,10 +13,12 @@ import okhttp3.OkHttpClient;
 public class EconomicApi extends AlphaVantageApi {
 
     private final GdpApi gdp;
+    private final GdpPerCapitaApi gdpPerCapita;
 
     public EconomicApi(AlphaVantageConfig config, OkHttpClient client) {
       super(config, client);
       this.gdp = new GdpApi(config, client);
+      this.gdpPerCapita = new GdpPerCapitaApi(config, client);
     }
 
 }
