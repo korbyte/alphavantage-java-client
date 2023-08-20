b9f519ccb70c97cc06876eda6c2fba5e85c3c555
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 00:26:18 -0400

    added intraday metadata

---
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayMetadata.java
@@ -0,0 +1,22 @@
+package com.korbyte.alphavantage.core.intraday;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.korbyte.alphavantage.base.BaseMetadata;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ToString(callSuper = true)
+public class IntradayMetadata extends BaseMetadata {
+
+  @JsonProperty(value = "4. Interval")
+  private String interval;
+
+  @JsonProperty(value = "5. Output Size")
+  private String outputsize;
+
+  @JsonProperty(value = "6. Time Zone")
+  private String timezone;
+}
