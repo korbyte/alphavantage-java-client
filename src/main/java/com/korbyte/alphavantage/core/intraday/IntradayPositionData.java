71261c334f28970c67168c5a3e52b8757333f86c
Gabe Nelson <unofficialwood@gmail.com>
2023-08-20 00:26:29 -0400

    added intraday position data

---
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayPositionData.java
@@ -0,0 +1,17 @@
+package com.korbyte.alphavantage.core.intraday;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.korbyte.alphavantage.base.BasePositionData;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.math.BigInteger;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ToString(callSuper = true)
+public class IntradayPositionData extends BasePositionData {
+  @JsonProperty(required = true, value = "5. volume")
+  private BigInteger volume;
+}
