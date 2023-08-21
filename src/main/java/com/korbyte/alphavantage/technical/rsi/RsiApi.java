c73bf74798202c689f3d6e5963ff83b7da094e03
Gabe Nelson <nelson.1616@osu.edu>
2023-08-21 17:25:37 -0400

    added comments and fixed rsi api

--- src/main/java/com/korbyte/alphavantage/technical/rsi/RsiApi.java
+++ src/main/java/com/korbyte/alphavantage/technical/rsi/RsiApi.java
@@ -1,17 +1,31 @@
 package com.korbyte.alphavantage.technical.rsi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
 import okhttp3.OkHttpClient;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * RSI API
+ * @see <a href="https://www.alphavantage.co/documentation/#rsi">RSI Documentation</a>
+ */
 public class RsiApi extends AlphaVantageApi  {
 
   public RsiApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
   }
 
-  public RsiResponse get(RsiParams params) throws Exception {
+  /**
+   * Get RSI data
+   * @param params RsiParams
+   * @return RsiResponse
+   * @throws Exception exception
+   */
+  public RsiResponse get(RsiParams params) throws URISyntaxException, IOException {
     String data = this.query(params);
-    return RsiResponse.from(data);
+    return new ObjectMapper().readValue(data, RsiResponse.class);
   }
 }
