ed6ebb52cc89374468f953d980d20256d43cee16
Gabe Nelson <nelson.1616@osu.edu>
2023-08-21 17:26:06 -0400

    added rsi response

--- src/main/java/com/korbyte/alphavantage/technical/rsi/RsiResponse.java
+++ src/main/java/com/korbyte/alphavantage/technical/rsi/RsiResponse.java
@@ -1,4 +1,18 @@
 package com.korbyte.alphavantage.technical.rsi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.korbyte.alphavantage.technical.rsi.models.RsiMetadata;
+import com.korbyte.alphavantage.technical.rsi.models.RsiPositionData;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.Map;
+
+@Data
 public class RsiResponse {
+  @JsonProperty("Meta Data")
+  private RsiMetadata metaData;
+
+  @JsonProperty("Technical Analysis: RSI")
+  private Map<Date, RsiPositionData> rsiPositionalData;
 }
