5850b086585166c76425786d12e467a4a9a432a0
Gabe Nelson <nelson.1616@osu.edu>
2023-08-25 16:41:32 -0400

    added javadoc

--- src/main/java/com/korbyte/alphavantage/core/daily/DailyApi.java
+++ src/main/java/com/korbyte/alphavantage/core/daily/DailyApi.java
@@ -8,6 +8,16 @@ import okhttp3.OkHttpClient;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+/**
+ * Daily API
+ * <p>
+ *   This API returns raw (as-traded) daily time series (date, daily open, daily high, daily low, daily close,
+ *   daily volume) of the global equity specified, covering 20+ years of historical data. The OHLCV data is sometimes
+ *   called "candles" in finance literature. If you are also interested in split/dividend-adjusted data, please use the
+ *   Daily Adjusted API, which covers adjusted close values and historical split and dividend events.
+ * </p>
+ * see <a href="https://www.alphavantage.co/documentation/#daily">Daily Api</a>
+ */
 public class DailyApi extends AlphaVantageApi {
   public DailyApi(AlphaVantageConfig config, OkHttpClient client) {
     super(config, client);
