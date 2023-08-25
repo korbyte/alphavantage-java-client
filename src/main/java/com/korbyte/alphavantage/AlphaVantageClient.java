f120c9e893eda9fd01d9f366f092e8551bd7a76e
Gabe Nelson <nelson.1616@osu.edu>
2023-08-25 16:45:41 -0400

    added in economic and technical

--- src/main/java/com/korbyte/alphavantage/AlphaVantageClient.java
+++ src/main/java/com/korbyte/alphavantage/AlphaVantageClient.java
@@ -1,7 +1,9 @@
 package com.korbyte.alphavantage;
 
 import com.korbyte.alphavantage.core.CoreApi;
+import com.korbyte.alphavantage.economic.EconomicApi;
 import com.korbyte.alphavantage.fundamental.FundamentalApi;
+import com.korbyte.alphavantage.technical.TechnicalApi;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import okhttp3.OkHttpClient;
@@ -12,7 +14,9 @@ import okhttp3.OkHttpClient;
 public class AlphaVantageClient {
 
   private final CoreApi core;
+  private final EconomicApi economic;
   private final FundamentalApi fundamental;
+  private final TechnicalApi technical;
 
   /**
    * Constructor
@@ -23,5 +27,7 @@ public class AlphaVantageClient {
     OkHttpClient client = new OkHttpClient();
     this.core = new CoreApi(config, client);
     this.fundamental = new FundamentalApi(config, client);
+    this.economic = new EconomicApi(config, client);
+    this.technical = new TechnicalApi(config, client);
   }
 }
