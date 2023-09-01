7f61c7ac3535632cb20da1027980d87b87004594
Gabe Nelson <unofficialwood@gmail.com>
2023-09-01 02:32:39 -0400

    fixed localdate output

--- src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusParams.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusParams.java
@@ -1,18 +1,20 @@
 package com.korbyte.alphavantage.fundamental.listing_status;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.korbyte.alphavantage.types.ApiFunction;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Accessors(chain = true)
 public class ListingStatusParams {
   @Setter(AccessLevel.NONE)
   private ApiFunction function = ApiFunction.LISTING_STATUS;
-  private Date date = new Date();
+  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+  private LocalDate date = LocalDate.now();
   private String state = "active";
 }
