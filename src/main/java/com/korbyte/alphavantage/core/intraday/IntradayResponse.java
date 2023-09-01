3054cc3a42b4d3973b45cbc3c9871677eb83935a
Gabe Nelson <unofficialwood@gmail.com>
2023-09-01 01:33:45 -0400

    reworked JsonAnySetter for ZonedDateTime Map

--- src/main/java/com/korbyte/alphavantage/core/intraday/IntradayResponse.java
+++ src/main/java/com/korbyte/alphavantage/core/intraday/IntradayResponse.java
@@ -1,12 +1,15 @@
 package com.korbyte.alphavantage.core.intraday;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.korbyte.alphavantage.core.intraday.models.IntradayMetadata;
 import com.korbyte.alphavantage.core.intraday.models.IntradayPositionData;
+import com.korbyte.alphavantage.util.date_time_deserializers.BaseZonedDateTimeBaseDeserializer;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -15,10 +18,20 @@ public class IntradayResponse {
   @JsonProperty("Meta Data")
   IntradayMetadata metadata;
 
-  Map<Date, IntradayPositionData> timeSeriesIntraday;
+  @JsonIgnore
+  Map<ZonedDateTime, IntradayPositionData> timeSeriesIntraday;
 
   @JsonAnySetter
-  public void setDynamicField(String key, Map<Date, IntradayPositionData> value) {
-    this.timeSeriesIntraday = value;
+  public void setDynamicField(String key, Map<String, IntradayPositionData> list) {
+    Map<ZonedDateTime, IntradayPositionData> dataMap = null;
+    if(key.contains("Time Series")){
+      dataMap = new HashMap<>();
+      for(String date : list.keySet()){
+        ZonedDateTime zonedDateTime = BaseZonedDateTimeBaseDeserializer.deserialize(date, "America/New_York");
+        dataMap.put(zonedDateTime, list.get(date));
+      }
+
+    }
+    this.timeSeriesIntraday = dataMap;
   }
 }
