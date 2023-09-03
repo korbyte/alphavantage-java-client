349a88f8c500ffb4027384e142be06fa0564ab6a
Gabe Nelson <unofficialwood@gmail.com>
2023-09-03 01:42:58 -0400

    Company Overview response updates

--- src/main/java/com/korbyte/alphavantage/fundamental/company_overview/CompanyOverviewApi.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/company_overview/CompanyOverviewApi.java
@@ -3,6 +3,7 @@ package com.korbyte.alphavantage.fundamental.company_overview;
 import com.korbyte.alphavantage.AlphaVantageApi;
 import com.korbyte.alphavantage.AlphaVantageConfig;
 import com.korbyte.alphavantage.error.ApiResponseException;
+import com.korbyte.alphavantage.fundamental.company_overview.models.CompanyOverview;
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
@@ -15,6 +16,9 @@ public class CompanyOverviewApi extends AlphaVantageApi {
 
   public CompanyOverviewResponse get(CompanyOverviewParams params) throws URISyntaxException, IOException, ApiResponseException {
     String data = this.query(params);
-    return this.parseResponse(data, CompanyOverviewResponse.class);
+    CompanyOverview overview = this.parseResponse(data, CompanyOverview.class);
+    CompanyOverviewResponse response = new CompanyOverviewResponse();
+    response.setCompanyOverview(overview);
+    return response;
   }
 }
--- src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusApi.java
+++ src/main/java/com/korbyte/alphavantage/fundamental/listing_status/ListingStatusApi.java
@@ -22,8 +22,6 @@ public class ListingStatusApi extends AlphaVantageApi {
     super(config, client);
   }
 
-  //todo figure out what happens when rate limit is hit and this method is called on the AV side
-
   /**
    * Get the listing status of all securities supported by Alpha Vantage
    *
